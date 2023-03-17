1736bdde55c1fcd49624a74ce5d6b0c7c5236291
vickyetty <vickyetty@yahoo.com>
2023-03-16 18:22:05 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,11 @@
 #include <stdio.h>
 /**
- * main - this is the main function 
+ * main -this is the main function 
  *
  * Return: always 0 (success)
  */
-
 int main(void)
 {
-	puts("\ "Programming is like building a multilingual puzzle");
+	puts('/"Programming is like building a multilingual puzzle");
 	return (0);
 }
