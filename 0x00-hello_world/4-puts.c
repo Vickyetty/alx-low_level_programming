08c468bc502d739737cab285a080f9f4983e6eb5
vickyetty <vickyetty@yahoo.com>
2023-03-16 19:06:21 -0700

    4-put

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,10 +2,10 @@
 /**
  * main -this is the main function 
  *
- * Return: always 0 (success)
+ * Return: always 0 
  */
 int main(void)
 {
-	puts('/"Programming is like building a multilingual puzzle");
-	return (0);
+	puts("\"programming is like building a multilingual puzzle");
+	return(0);
 }
