b47ba2676250b6acc878d9f52b4ad709d7483abd
vickyetty <vickyetty@yahoo.com>
2023-03-21 17:59:50 -0700

    Functions, nested loops

---
+++ 0x02-functions_nested_loops/10-main.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    int n;
+
+    n = add(89, 9);
+    printf("%d\n", n);
+    return (0);
+}
+julien@ubuntu:~/0x02$ gcc -Wall -pedantic -Werror -Wextra -std=gnu89 _putchar.c 10-main.c 10-add.c -o 10-add
+julien@ubuntu:~/0x02$ ./10-add 
+98
+julien@ubuntu:~/0x02$ 
